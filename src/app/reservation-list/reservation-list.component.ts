b1f0d3971a7879713120d268d209acc7ce2e20cb
James Florez <jandrey2002@gmail.com>
2024-02-20 17:07:55 -0500

    Se implementa Angular RxJS

--- src/app/reservation-list/reservation-list.component.ts
+++ src/app/reservation-list/reservation-list.component.ts
@@ -1,30 +1,47 @@
-// Parte 2 Formulario/ parte 4 inyeccion de dependencias
-import { Component } from '@angular/core';
+
+import { Component, OnInit } from '@angular/core';
 import { ReservationStorageService } from '../reservation-storage.service';
+import { Reserva } from '../reservation/reservation.interface';
+import { Observable, of } from 'rxjs';
+
 
 @Component({
   selector: 'app-reservation-list',
   templateUrl: './reservation-list.component.html',
   styleUrls: ['./reservation-list.component.css']
 })
-export class ReservationListComponent {
-  reservations: any[];
+export class ReservationListComponent implements OnInit{
+  
+  reservas: Reserva[] =[];
   bookTitle: string = "";
   customerName: string = "";
   date: Date = new Date();
+  reserva1: Observable<Reserva[]>;
+
+  
 
   constructor(private reservationStorageService: ReservationStorageService) {
-    this.reservations = this.reservationStorageService.getReservations();
+    this.reservas = this.reservationStorageService.getReservations();
+    this.reservas.push(
+      { id: 1, bookTitle: 'Libro 1', customerName: 'Cliente 1', date: new Date() },
+      { id: 2, bookTitle: 'Libro 2', customerName: 'Cliente 2', date: new Date() },
+      { id: 3, bookTitle: 'Libro 3', customerName: 'Cliente 3', date: new Date() }
+    );
+    this.reserva1 = of(this.reservas);
   }
 
+  ngOnInit(): void {
+    this.reserva1.subscribe((reservations: Reserva[]) => console.log(this.reservas));
+  }
   onSubmit(): void {
-    const newReservation = {
+    const newReservation : Reserva ={
+      id: this.reservas.length + 1,
       bookTitle: this.bookTitle,
       customerName: this.customerName,
       date: this.date
     };
-    this.reservations.push(newReservation);
-    this.reservationStorageService.saveReservations(this.reservations);
+    this.reservas.push(newReservation);
+    this.reservationStorageService.saveReservations(this.reservas);
 
     this.bookTitle = "";
     this.customerName = "";
@@ -32,9 +49,8 @@ export class ReservationListComponent {
   }
 
   deleteReservation(index: number): void {
-  
-    this.reservations.splice(index, 1);
-    this.reservationStorageService.saveReservations(this.reservations);
+    this.reservas.splice(index, 1);
+    this.reservationStorageService.saveReservations(this.reservas);
   }
 }
 
---
+++ src/app/reservation/reservation.interface.ts
@@ -0,0 +1,7 @@
+export interface Reserva {
+    id: number;
+    bookTitle: string;
+    customerName: string;
+    date: Date;
+    
+  }
